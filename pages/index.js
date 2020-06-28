b753b20dd9182deabb16da9355f297671fd19868
scdev0 <scdev22@gmail.com>
2020-06-27 23:46:49 -0400

    Add date-fns and links to posts

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,10 @@ import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import { getSortedPostsData } from '../lib/posts';
+import Link from 'next/link';
+import Date from '../components/date';
 
-export default function Home({ allPostsData }) {
+export default function Home({ sortedPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -19,13 +21,15 @@ export default function Home({ allPostsData }) {
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
+          {sortedPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href='/posts/[id]' as={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
@@ -35,10 +39,10 @@ export default function Home({ allPostsData }) {
 }
 
 export async function getStaticProps() {
-  const allPostsData = getSortedPostsData();
+  const sortedPostsData = getSortedPostsData();
   return {
     props: {
-      allPostsData,
+      sortedPostsData,
     },
   };
 }
