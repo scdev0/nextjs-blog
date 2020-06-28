8acfc6c781a461baa34f9fc728891097c34944ed
scdev0 <scdev22@gmail.com>
2020-06-28 00:25:03 -0400

    Test preview

--- pages/index.js
+++ pages/index.js
@@ -13,10 +13,7 @@ export default function Home({ sortedPostsData }) {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>Hi I'm scdev and this is my blog built with next.js</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href='https://nextjs.org/learn'>our Next.js tutorial</a>.)
-        </p>
+        <p>Testing Preview</p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
